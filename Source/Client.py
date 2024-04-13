e76fa8a582d9bdbf44e0a30f5a8f1ceaf137bb41
zahrahojati19 <129644190+zahrahojati19@users.noreply.github.com>
2024-04-13 21:19:26 +0330

    Client.py

--- Source/Client.py
+++ Source/Client.py
@@ -4,7 +4,7 @@ import order_management_pb2_grpc
 
 def run():
     with grpc.insecure_channel("localhost:50051") as channel:
-        stub = order_management_pb2_grpc.OrderManagementStub(channel)
+        client = order_management_pb2_grpc.OrderManagementStub(channel)
 
         while True:
             print("1. Get Order - Unary")
@@ -15,11 +15,11 @@ def run():
             
             if choice == "1":
                 order_name = input("Enter order name: ")
-                response = stub.getOrder(order_management_pb2.OrderRequest(order_name=order_name))
+                response = client.getOrder(order_management_pb2.OrderRequest(order_name=order_name))
                 print(response)
             elif choice == "2":
                 num_messages = int(input("Enter number of messages: "))
-                responses = stub.searchOrders(order_management_pb2.ReceiveMessagesRequest(num_messages=num_messages))
+                responses = client.searchOrders(order_management_pb2.ReceiveMessagesRequest(num_messages=num_messages))
                 for response in responses:
                     print(response)
             elif choice == "3":
@@ -29,7 +29,7 @@ def run():
                     if not message:
                         break
                     messages.append(message)
-                response = stub.updateOrders(iter([order_management_pb2.UploadMessagesRequest(messages=messages)]))
+                response = client.updateOrders(iter([order_management_pb2.UploadMessagesRequest(messages=messages)]))
                 print(response)
             elif choice == "4":
                 messages = []
@@ -38,7 +38,7 @@ def run():
                     if not message:
                         break
                     messages.append(message)
-                responses = stub.processOrders(iter([order_management_pb2.ChatMessage(message=message) for message in messages]))
+                responses = client.processOrders(iter([order_management_pb2.ChatMessage(message=message) for message in messages]))
                 for response in responses:
                     print(response)
             else:
